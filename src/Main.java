3bcbfc156f8efb35cd20da3a25f73a66cb27c1fa
Alceu Neto <alceuflauta@gmail.com>
2026-02-14 18:24:59 -0300

    Novo Quiz! praticando

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,44 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
-    public  static void main (String[] args) {
+    public static void main(String[] args) {
+        String[] perguntas = {"Que ano o Alceu nasceu?", "Quantos anos ele tem agora?", "Quantos ele vai fazer? "};
 
-        String[] questoesquiz = {"Quanto é 10 + 10?", "Quanto é 5x5?", "Quanto é 9x8?"};
+        String[][] opcoes = {{"1. 2003", "2. 2002", "3. 2004"
+        }, {"1. 22", "2. 24", "3. 25"},
+                {"1. 23", "2. 24", "3. 25"}};
 
-        String[][] respostas = {{"1. 20", "2. 15", "3. 25"}, {"1. 15", "2. 25", "3. 45"},
-                {"1. 72", "2. 81", "3. 64"}};
 
-        Scanner scanner = new Scanner(System.in);
-        String desejar;
+        String deseja;
         do {
-            int pontuacao = 0;
-            System.out.println("Sejam bem-Vindo ao quiz!");
-            int respostascertas[] = {1, 2, 1};
 
+            Scanner scanner = new Scanner(System.in);
+            int pontuaacao = 0;
+            int[] respostas = {1, 1, 1};
 
-            for (int i = 0; i < questoesquiz.length; i++) {
-                System.out.println(questoesquiz[i]);
-
-                for (String opcao : respostas[i]) {
+            System.out.println("Bem-Vindo ao Quiz do Alceu!");
+            for (int i = 0; i < perguntas.length; i++) {
+                System.out.println(perguntas[i]);
+                for (String opcao : opcoes[i]) {
                     System.out.println(opcao);
-
                 }
-                ;
-                System.out.println("De sua resposta:");
-                int responder = scanner.nextInt();
-                if (responder == respostascertas[i]) {
+                System.out.println("Dê sua resposta: ");
+                int guess = scanner.nextInt();
+
+                if (guess == respostas[i]) {
                     System.out.println("Certa resposta!");
-                    pontuacao++;
-
+                    pontuaacao++;
                 } else {
-                    System.out.println("Resposta errada!, a resposta era: " + respostascertas[i]);
+                    System.out.println("Errado! A resposta era: " + respostas[i]);
                 }
-
-
+                System.out.println("Fim do quiz! Sua pontuação foi de: " + pontuaacao);
+                System.out.println("Deseja jogar novamente? (S/N");
+                scanner.nextLine();
 
             }
-            System.out.println("Quiz finalizado!, sua pontuação foi de: " + pontuacao + "," + "de: " + questoesquiz.length);
-            scanner.nextLine();
-            System.out.println("Deseja jogar novamente? S/N");
-            desejar = scanner.nextLine();
+            deseja = scanner.nextLine();
 
-
-
-        }while(desejar.equalsIgnoreCase("S"));
-        System.out.println("Até a próxima!");
-
-
-    }
-
-}
+        } while (deseja.equalsIgnoreCase("S"));
+        System.out.println("De boas, até  a próxima!");
+    }}
